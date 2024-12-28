6e057709597e187940d3ed2b8ec3f54a8a899eae
Arkadiusz Witasik <arkadiuszwitasik.kontakt@gmail.com>
2024-12-28 16:19:24 +0100

    Change project name to english version

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { IconChristmasTreeFilled } from '@tabler/icons-react';
 
 const Cell = () => {
-	return <div className="w-[24px] h-[24px] border border-white">x</div>;
+	return (
+		<div className="w-[24px] h-[24px] border border-white flex justify-center items-center">
+			<IconChristmasTreeFilled size={16} />
+		</div>
+	);
 };
 
 export default Cell;
